f529fc1ecefbae23903124bc03629af508d30633
devintang <yonghua_t@qq.com>
2021-04-16 09:53:44 +0800

    tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view></router-view>
+    <main-tab-bar/>
   </div>
 </template>
 
+<script>
+import MainTabBar from 'components/content/MainTabBar'
+
+export default {
+  name: 'app',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
 <style lang="scss">
-@import "./assets/css/base.css";
+@import "assets/css/base.css";
 
 </style>
